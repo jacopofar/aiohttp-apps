edef160500247a0e1e7214c748626bd495350a8e
jacopofar <jacopo1.farina@gmail.com>
2017-04-05 17:55:38 +0200

    load config from a module

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from aiohttp import web
 from pastabin import pastabin
-
+import config
 app = web.Application()
-
+app['config'] = config
+# load global configuration
+# nested applications are in their respective folder, to make easier to split them
 app.add_subapp('/pastabin/', pastabin.PastabinApp().get_app())
 
 
-web.run_app(app, port=8080)
+web.run_app(app, port=config.web_port)
---
+++ config.py
@@ -0,0 +1,2 @@
+# example, this file will load the configuration
+web_port = 8080
