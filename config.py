1b028345457d53adf9ed459961ad5762dde7c07a
jacopofar <jacopo1.farina@gmail.com>
2017-04-12 15:19:41 +0200

    default config

--- config.py
+++ config.py
@@ -10,6 +10,12 @@ print(f'\n\n+++ the secret token is {secret_token} +++\n\n')
 
 config = configparser.ConfigParser()
 config.read('config.ini')
-jwt_secret = config['JWT']['jwt_secret']
-jwt_algorithms = config['JWT']['jwt_algorithms'].split(' ')
+jwt_secret = secret_token
+jwt_algorithms = ['HS256']
+
+if 'JWT' not in config:
+    print('WARNING, no config.ini file, secret token will be used for JWT validation instead...')
+else:
+    jwt_secret = config['JWT']['jwt_secret']
+    jwt_algorithms = config['JWT']['jwt_algorithms'].split(' ')
 
